c2b00516519d83c221e3923c5605442f9ec5b810
Mikhail <mha.willy@gmail.com>
2022-09-18 20:40:37 +0700

    task004

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите положительное число");
+int a = int.Parse(Console.ReadLine());
+int count = 2;
+
+while(count <= a)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
+
