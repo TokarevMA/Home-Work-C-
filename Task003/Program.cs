064e19b92766281c21c5a45074c0997ae0a3c5b7
Mikhail <mha.willy@gmail.com>
2022-09-18 20:38:42 +0700

    task003

---
+++ Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+string input1 = Console.ReadLine();
+int a = int.Parse(input1);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
